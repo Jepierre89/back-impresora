249fd5eb839dba897c02e38fad7ca9cb5b1bbdd7
Jepierre88 <jeanpierre.ortiz@coins-colombia.com>
2025-05-08 10:39:47 -0500

    Cambio a 8080

--- src/main/java/com/example/App.java
+++ src/main/java/com/example/App.java
@@ -33,7 +33,7 @@ import java.util.Map; // Para manejar los datos del JSON
 
 public class App {
     public static void main(String[] args) {
-        Spark.port(8081);
+        Spark.port(8080);
         Spark.after((request, response) -> {
             response.header("Access-Control-Allow-Origin", "*");
             response.header("Access-Control-Allow-Methods", "*");
